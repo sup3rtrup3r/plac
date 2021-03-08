3f2bf3a570fb1e5309d5e03546370ff6da72b4be
sup3rtrup3r <skoda.ivan988@gmail.com>
2021-03-08 14:55:54 +0100

    Using VegetablesFactory in a test

--- plac/povrce/tests/test_models.py
+++ plac/povrce/tests/test_models.py
@@ -1,12 +1,11 @@
 import pytest
 
-from ..models import Povrce
+from .factories import VegetablesFactory
 
 pytestmark = pytest.mark.django_db
 
 
 def test__str__():
-    povrce = Povrce.objects.create(name='luk', description='od luka se fuka',
-                                   vrsta=Povrce.Vrsta.LUKOVICASTO)
-    assert povrce.__str__() == 'luk'
-    assert str(povrce) == 'luk'
+    povrce = VegetablesFactory()
+    assert povrce.__str__() == povrce.name
+    assert str(povrce) == povrce.name

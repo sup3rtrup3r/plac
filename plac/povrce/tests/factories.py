2be27e66b10fb51e7a865858a066b8a929a27151
sup3rtrup3r <skoda.ivan988@gmail.com>
2021-03-08 14:01:30 +0100

    Add a vegetables factory for making dummy vegetables

---
+++ plac/povrce/tests/factories.py
@@ -0,0 +1,20 @@
+from django.template.defaultfilters import slugify
+
+import factory
+import factory.fuzzy
+
+from ..models import Povrce
+
+
+class VegetablesFactory(factory.django.DjangoModelFactory):
+    name = factory.fuzzy.FuzzyText()
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
+    description = factory.Faker(
+        'paragraph', nb_sentences=3, variable_nb_sentences=True
+    )
+    vrsta = factory.fuzzy.FuzzyChoice(
+        [x[0] for x in Povrce.Vrsta.choices]
+    )
+
+    class Meta:
+        model = Povrce
